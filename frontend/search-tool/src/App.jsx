f8fcc19ad02d311f7f6a718304f9b4ce24f8c0bd
stephtheitsloth <stephan.karim.sk@gmail.com>
2024-03-02 14:38:47 -0500

    update branch

--- frontend/search-tool/src/App.jsx
+++ frontend/search-tool/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+=======
+      <h1 className="text-3xl font-bold underline">
+      Search Engine
+    </h1>
+    <SearchForm />
+>>>>>>> Stashed changes
     </>
   )
 }
