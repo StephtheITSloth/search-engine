a7d51168f44aaab9839528f55a4338e3eabb0116
stephtheitsloth <stephan.karim.sk@gmail.com>
2024-03-03 11:28:42 -0500

    implementation: added react query to improve fetching logic

--- frontend/search-tool/src/components/SearchForm.jsx
+++ frontend/search-tool/src/components/SearchForm.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useFormik} from "formik"
 import * as Yup from "yup"
+import {useQuery} from '@tanstack/react-query'
+import { axiosInstance } from '../axiosInstance'
 
 const searchFormValidationSchema = Yup.object().shape({
     searchInput: Yup.string()
@@ -9,22 +11,30 @@ const searchFormValidationSchema = Yup.object().shape({
     .required('Input is Required')
 })
 
+async function fetchData(){
+    const {data} = await axiosInstance.get()
+    return data
+}
+
+
 function SearchForm() {
+    const [searchData, setSearchData] = useState([])
+
+    const fallback = []
+
+    const {data = fallback, isLoading} = useQuery({
+    queryKey: ['search'],
+    queryFn: fetchData
+    });
+
     const {values, handleBlur, handleChange, handleSubmit, errors, touched, isSubmitting, isValidating} = useFormik({
         initialValues: {
             searchInput: ""
         },
         validationSchema: searchFormValidationSchema,
         validateOnChange: true,
-        onSubmit: values => {
-
-            // //guard for validation
-            // if(isValidating && isSubmitting){
-            //     console.log("is Valid")
-            // }
-
-            console.log("is valid")
-
+        onSubmit: async (values) => {
+            await fetchData()
         },
         validate: values => {
             let errors = {}
@@ -36,14 +46,18 @@ function SearchForm() {
         }
     })
   return (
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="searchInput">{errors.searchInput && <div>{errors.searchInput}</div>}</label>
-        <input type="text" placeholder="Enter your search value" onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.searchInput}
-             name="searchInput"/>
-        <button type="submit">search</button>
-    </form>
+    <>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="searchInput">{errors.searchInput && <div>{errors.searchInput}</div>}</label>
+            <input type="text" placeholder="Enter your search value" onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.searchInput}
+                name="searchInput"/>
+            <button type="submit">search</button>
+        </form>
+        {isLoading && (<h3>Loading...</h3>)}
+        {data.map(elem => {<p key={elem.id}>{elem.title}</p>})}
+    </>
   )
 }
 
