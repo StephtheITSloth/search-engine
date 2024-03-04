2d30beeed7d614404689fdd90063df4765db3bb6
stephtheitsloth <stephan.karim.sk@gmail.com>
2024-03-04 02:51:46 -0500

    uploaded data to elastic

--- backend/venv/server.py
+++ backend/venv/server.py
@@ -1,38 +1,18 @@
 
-# /////////////////////////////////
 
 from flask import Flask
 from flask import jsonify, request
-from elasticsearch import Elasticsearch
-from elasticsearch import helpers
-from dotenv import load_dotenv
-import os
+from elasticsearch_connection import es
 
-load_dotenv()
+
 app = Flask(__name__)
 
-# app.config['USERNAME'] = os.getenv('USERNAME')
-# app.config['PASSWORD'] = os.getenv('PASSWORD')
-# app.config['CLOUD_ID'] = os.getenv('SECRET_KEY')
-
-cloud_id = "be6ec3baaca345a3a87d741fac6fe7cf:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDg1NmNiNjJlNTA5ZjQ1OTRhN2RmNmI4MDZhM2FiNjAxJDJkNGQwNjM3NmQxYzRjZGNhMzE1NTQ3YWNhMzUzYzNm"
-username = "rimkstylish1"
-password = "Stephrimk1$"
-es = Elasticsearch(cloud_id=cloud_id, http_auth=(username, password))
 
 response = es.cluster.health()
 print(response)
 
-# ////////////////////////
-
-
-
-
-es = Elasticsearch(cloud_id=cloud_id, http_auth=(username, password))
-
-
-
-# load_dotenv()
+es.search(index="search-payment-csv",body={"query": {"match_all": {}}})
+print("Documents Found")
 
 
 # from api.insert_data import 
@@ -40,7 +20,7 @@ es = Elasticsearch(cloud_id=cloud_id, http_auth=(username, password))
 def home():
     print('Server is running')
 
-
+es.search(index="search-paymentdata2024", q="snow")
 # @app.route('/get_user', methods=['GET'])
 # def get_user():
 #     user_id = request.form['id']
